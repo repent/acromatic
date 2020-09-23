f751d92ebe641235e611d3bba953c37cab96f721
Dan Hetherington <dan.hetherington@gmail.com>
2020-09-23 18:25:52 +0100

    Basic seed file for development to aid testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+case Rails.env
+when "development"
+
+user = User.create! email: 'dan.hetherington@gmail.com', password: 'password', password_confirmation: 'password'
+
+dictionaries = Dictionary.create(
+  [{name: 'Words and that'}])
+
+definitions = Definition.create([
+  {initialism: 'AA', dictionary: dictionaries.first, meaning: 'Automobile Association'},
+  {initialism: 'AAA', dictionary: dictionaries.first, meaning: 'Alcoholics Anonymous'},
+  {initialism: 'BB', dictionary: dictionaries.first, meaning: "Beautiful booty"},
+  {initialism: 'BB', dictionary: dictionaries.first, meaning: "Bountiful beauty"}
+])
+
+end
